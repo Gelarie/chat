b15b2da70fccd4763261d839b10372499697a900
Gelarie <kavtaskinp@gmail.com>
2017-05-17 00:11:22 +0500

    Add files via upload

---
+++ chat/Client/Login.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Login : Form
+    {
+        public Client Client { get; set; }
+
+        public Login()
+        {
+            Client = new Client();
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            int port = int.Parse(textBox1.Text);
+            Client.Connected += Client_Connected;
+            Client.Connect(txtIP.Text, port);
+            Client.Send("Connect|" + txtNickname.Text + "|connected");
+        }
+
+        private void Client_Connected(object sender, EventArgs e)
+        {
+            this.Invoke(Close);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
